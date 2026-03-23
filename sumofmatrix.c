701842fc68bc196265867425c9d89cff5b17e52f
Jinisha Jain <jinisharjain@gmail.com>
2026-03-23 17:10:04 +0530

    Add sum of matrix

---
+++ sumofmatrix.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int matrix[20][20];
+    int i,j,r,c;
+    scanf("%d",&r); 
+    scanf("%d",&c);
+    for(i=0;i< r;i++) 
+    {
+        for(j=0;j< c;j++)
+        {
+            scanf("%d",&matrix[i][j]); 
+        }
+    }
+   int sum;
+   for(i=0;i<r ; i++)
+   {
+     sum=0;
+     for(j=0;j<c; j++)
+     {
+       sum = sum + matrix[i][j];
+     }
+     printf("%d\n", sum);
+   }
+}
